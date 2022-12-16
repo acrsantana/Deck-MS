a8c1b30b0709149d54d55cae72ea60f6952073e6
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-12-16 18:52:45 -0300

    Correção no feignclient

--- src/main/java/br/com/acrteck/planningpoker/decks/http/CartasClient.java
+++ src/main/java/br/com/acrteck/planningpoker/decks/http/CartasClient.java
@@ -1,15 +1,14 @@
 package br.com.acrteck.planningpoker.decks.http;
 
 import br.com.acrteck.planningpoker.decks.dto.CartaDto;
-import br.com.acrteck.planningpoker.decks.model.Organizacao;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient("cartas")
+@FeignClient("http://localhost:8080/cartas/planningpoker/api/v1/cartas")
 public interface CartasClient {
-    @GetMapping(value = "/cartas/planningpoker/api/v1/cartas/{organizacao}")
-    List<CartaDto> obtemCartasPorOrganizacao(@PathVariable Organizacao organizacao);
+    @GetMapping(value = "/organizacao/{idOrganizacao}")
+    List<CartaDto> obtemCartasPorOrganizacao(@PathVariable Integer idOrganizacao);
 }
